f77475ae2acf200b9fb2bdec196bd5c9298cb16c
Davyd_Yukhymchuk <120454887+Davyd-Yu@users.noreply.github.com>
2023-05-21 17:28:33 +0300

    LAB_7_UPDATE

--- main.py
+++ main.py
@@ -1,33 +1,62 @@
+"""
+This is 7-th lab where I created class Bicycle
+"""
+
+
 class Bicycle:
+    """
+    class which creates bicycles and redacts their current speed
+    """
     instance = None
+
+    def __init__(self, type_of_bicycle='', brand='', max_speed=0, cur_speed=0):
+        """
+        :param type_of_bicycle: type of my bicycle
+        :param brand: brand of my bicycle
+        :param max_speed: max speed of my bicycle
+        :param cur_speed: current speed of my bicycle
+        Constructor initializes parameters
+        """
+        self.type_of_bicycle = type_of_bicycle
+        self.brand = brand
+        self.max_speed = max_speed
+        self.cur_speed = cur_speed
+
+    def __str__(self):
+        """
+        :return: returns converted in string object
+        """
+        return f"type: {self.type_of_bicycle}, brand: {self.brand}," \
+               f" max_speed: {self.max_speed}, cur_speed: {self.cur_speed}"
+
+    def accelerate(self, speed):
+        """
+        :param speed: seed of bicycle
+        :return: returns accelerated bicycle's speed
+        """
+        if speed < self.max_speed:
+            self.cur_speed += speed
+        return self.cur_speed
+
+    def brake(self):
+        """
+        :return: returns 0 as current speed
+        """
+        self.cur_speed = 0
+        return self.cur_speed
 
     @staticmethod
     def get_instance():
+        """
+        :return: this is static method to returning None for typical bicycle
+        """
+        if not Bicycle.instance:
+           Bicycle.instance = Bicycle("typical", "typical", 0, 0)
         return Bicycle.instance
 
-    def __init__(self, type, brand, max_speed, current_speed):
-        self.type = type
-        self.brand = brand
-        self.max_speed = max_speed
-        self.current_speed = current_speed
 
-    def accelerate(self, speed):
-        if speed < self.max_speed:
-            self.current_speed += speed
-        return self.current_speed
-
-    def brake(self):
-        self.current_speed = 0
-        return self.current_speed
-
-    def increase_price(self, price):
-        return price + 50
-
-    def __str__(self):
-        return f"type: {self.type}, brand: {self.brand}, max_speed: {self.max_speed}, current_speed: {self.current_speed}"
-
-
-bicycles = [Bicycle.get_instance(), Bicycle("mountain", "Marta", 45, 34)]
+bicycles = [Bicycle.get_instance(), Bicycle("mountain", "Marta", 45, 34), Bicycle()]
 
 for bicycle in bicycles:
     print(bicycle)
+
